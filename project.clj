043361d4ec1503b4def1c3e5a51f8397aa2cfac1
Paula Gearon <pgearon@cisco.com>
2016-07-28 18:27:42 -0400

    Initial commit, brought in from kanonas

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject kanonas "0.1.0-SNAPSHOT"
+(defproject naga "0.1.0-SNAPSHOT"
   :description "Forward Chaining Rule Engine"
-  :url "http://github.com/quoll/kanonas"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "http://github.com/threatgrid/naga"
+  #_:license #_{:name "Eclipse Public License"
+                :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [prismatic/schema "1.0.5"]])
