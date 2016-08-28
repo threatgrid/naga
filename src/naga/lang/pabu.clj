e0d5751b058e04fdc842dcd0ac7612144afc8a5f
Paula Gearon <pgearon@cisco.com>
2016-08-28 01:56:10 -0400

    Added cli to run a program

--- src/naga/lang/pabu.clj
+++ src/naga/lang/pabu.clj
@@ -2,17 +2,67 @@
 Parses code and returns Naga rules."
       :author "Paula Gearon"}
   naga.lang.pabu
-  (:require [naga.schema.structs :as structs]
-            [naga.lang.parser :as parser]))
+  (:require [naga.schema.structs :as structs :refer [Axiom Program]]
+            [naga.lang.parser :as parser]
+            [naga.rules :as r]
+            [schema.core :as s])
+  (:import [java.io InputStream]
+           [naga.schema.structs Rule]))
 
-(defn read-str
+;; TODO: Multi-arity not yet supported
+(def Args
+  [(s/one s/Any "entity")
+   (s/optional s/Any "value")])
+
+(def AxiomAST
+  {:type :axiom
+   :axiom [(s/one s/Keyword "Property")
+           (s/one Args "args")]})
+
+(def Triple
+  [(s/one s/Any "entity")
+   (s/one s/Any "property")
+   (s/one s/Any "value")])
+
+(s/defn triplet :- Triple
+  [[property [s o :as args]]]
+  (if (= 1 (count args))
+    [s :rdf/type property]
+    [s property o]))
+
+(s/defn ast->axiom :- Axiom
+  "Converts the axiom structure returned from the parser"
+  [{axiom :axiom :as axiom-ast} :- AxiomAST]
+  (triplet axiom))
+
+(def VK (s/cond-pre s/Keyword s/Symbol))
+
+(def Predicate [(s/one VK "property")
+                (s/one Args "arguments")])
+
+(def RuleAST
+  {:type :rule
+   :head [(s/one VK "property")
+          (s/one Args "arguments")]
+   :body [Predicate]})
+
+(s/defn ast->rule
+  "Converts the rule structure returned from the parser"
+  [{:keys [head body] :as rule-ast} :- RuleAST]
+  (r/rule (triplet head) (map triplet body) (gensym (name (first head)))))
+
+(s/defn read-str :- {:rules [Rule]
+                     :axioms [Axiom]}
   "Reads a string"
-  [s]
-  (let [program (parser/parse s)]
-    program))
+  [s :- s/Str]
+  (let [program-ast (parser/parse s)
+        axioms (filter (comp (partial = :axiom) :type) program-ast)
+        rules (filter (comp (partial = :rule) :type) program-ast)]
+    {:rules (map ast->rule rules)
+     :axioms (map ast->axiom axioms)}))
 
-(defn read-stream
+(s/defn read-stream :- Program
   "Reads a input stream"
-  [in]
+  [in :- InputStream]
   (let [text (slurp in)]
     (read-str text)))
