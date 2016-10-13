636687bf34288f2f2246ee9b83f7dafc9a7a22b3
Paula Gearon <pgearon@cisco.com>
2016-10-13 16:31:25 -0400

    CLI operating on JSON

--- src/naga/cli.clj
+++ src/naga/cli.clj
@@ -9,15 +9,21 @@
             [naga.engine :as e]
             [naga.store :as store]
             [naga.data :as data]
-            [naga.storage.memory.core]))
+            [naga.storage.memory.core])
+  (:import [java.io File]))
 
 (def stores (map name (keys @store/registered-stores)))
 
+(def valid-output? #(.exists (.getParentFile (.getAbsoluteFile (File. %)))))
+(def valid-input? #(.exists (File. %)))
+
 (def cli-options
-  [["-s" "--storage" "Select store type"
+  [[nil "--storage STRING" "Select store type"
     :validate [(set stores) "Must be a registered storage type."]]
-   ["-j" "--json" "Filename for input json"]
-   ["-o" "--output" "Filename for output json"]
+   [nil "--json STRING" "Filename for input json" :default "in.json"
+    :validate [valid-input? "Input file does not exist."]]
+   [nil "--output STRING" "Filename for output json" :default "out.json"
+    :validate [valid-output? "Output directory does not exist."]]
    ["-h" "--halp" "Print help"]])
 
 (defn exit
@@ -96,12 +102,13 @@
 
 (defn json-program
   "Runs a program over data in a JSON file"
-  [in-stream json-file storage]
-  (let [json (data/stream->triples json-file)
-        ; TODO: handle storage URIs to determine type and connection
+  [in-stream json-file out-file storage]
+  (let [; TODO: handle storage URIs to determine type and connection
         ; fresh-store (instantiate-storage storage)
         fresh-store (store/get-storage-handle {:type :memory})
         {:keys [rules axioms]} (pabu/read-stream in-stream)
+
+        json (data/stream->triples fresh-store json-file)
 
         basic-store (store/assert-data fresh-store axioms)
         json-data (data/stream->triples basic-store json-file)
@@ -113,11 +120,12 @@
         program (r/create-program rules [])
 
         ;; run the program
-        [store stats] (e/run config program)]
-    (data/store->str store)))
+        [store stats] (e/run config program)
+        output (data/store->str store)]
+    (spit out-file output)))
 
 (defn -main [& args]
-  (let [{{:keys [halp json out storage]} :options,
+  (let [{{:keys [halp json output storage]} :options,
          arguments :arguments :as opts} (parse-opts args cli-options)]
 
     (when halp (exit 1 (usage opts)))
@@ -125,5 +133,5 @@
                             (io/input-stream filename)
                             *in*)]
       (if json
-        (json-program in-stream json storage)
+        (json-program in-stream json output storage)
         (logic-program in-stream)))))
