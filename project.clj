4bae248f700bc842980db49b50ac32379824823b
Paula Gearon <pgearon@cisco.com>
2017-07-21 18:34:12 -0400

    Handling independent entities in rules in Datomic

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.quoll/naga "0.2.1-SNAPSHOT"
+(defproject org.clojars.quoll/naga "0.2.1"
   :description "Forward Chaining Rule Engine"
   :url "http://github.com/threatgrid/naga"
   :license {:name "Eclipse Public License"
