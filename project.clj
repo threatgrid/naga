a4f0c7c9cb74b1c7dcd6d169aca699170ce9c770
Paula Gearon <pgearon@cisco.com>
2018-06-19 20:56:32 -0400

    Updated dependencies to snapshots in progress

--- project.clj
+++ project.clj
@@ -10,8 +10,8 @@
                  [org.clojure/core.cache "0.7.1"]
                  [the/parsatron "0.0.7"]
                  [cheshire "5.8.0"]
-                 [org.clojars.quoll/naga-store "0.1.0"]
-                 [org.clojars.quoll/asami "0.2.3"]
+                 [org.clojars.quoll/naga-store "0.2.0-SNAPSHOT"]
+                 [org.clojars.quoll/asami "0.2.4-SNAPSHOT"]
                  ; [com.datomic/datomic-pro "0.9.5697" :exclusions [com.google.guava/guava] ; uncomment for Datomic Pro
                  [com.datomic/datomic-free "0.9.5697" :exclusions [com.google.guava/guava]]
                  [org.postgresql/postgresql "9.3-1102-jdbc41"]]
