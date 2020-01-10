3030c4158365f369f00e8d08cbd3890d0f978b8d
Paula Gearon <pgearon@cisco.com>
2020-01-10 12:40:12 -0500

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.quoll/naga "0.2.27"
+(defproject org.clojars.quoll/naga "0.2.28"
   :description "Forward Chaining Rule Engine"
   :url "http://github.com/threatgrid/naga"
   :license {:name "Eclipse Public License"
