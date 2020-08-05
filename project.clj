1e7cb4322ebcb72f9efd050608126f818f8044dc
Paula Gearon <pgearon@cisco.com>
2020-08-05 16:58:42 -0400

    Bumped dependencies for release

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(defproject org.clojars.quoll/naga "0.3.0-SNAPSHOT"
+(defproject org.clojars.quoll/naga "0.3.0"
   :description "Forward Chaining Rule Engine"
   :url "http://github.com/threatgrid/naga"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.597"]
+                 [org.clojure/clojurescript "1.10.773"]
                  [prismatic/schema "1.1.12"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/core.cache "0.7.1"]
                  [org.clojars.quoll/parsatron "0.0.9"]
                  [cheshire "5.10.0"]
-                 [org.clojars.quoll/naga-store "0.3.5-SNAPSHOT"]
-                 [org.clojars.quoll/zuko "0.1.0-SNAPSHOT"]
-                 [org.clojars.quoll/asami "1.0.0-SNAPSHOT"]
+                 [org.clojars.quoll/naga-store "0.3.6"]
+                 [org.clojars.quoll/zuko "0.2.2"]
+                 [org.clojars.quoll/asami "1.1.0"]
                  [org.clojars.quoll/qtest "0.1.1"]
                  ; [com.datomic/datomic-pro "0.9.5697" :exclusions [com.google.guava/guava] ; uncomment for Datomic Pro
                  [com.datomic/datomic-free "0.9.5697" :exclusions [com.google.guava/guava]]
